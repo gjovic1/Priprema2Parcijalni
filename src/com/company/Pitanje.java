7c4d130eb88bb15716fa73055186a49ebff2d5c5
gjovic1 <gjovic1@etf.unsa.ba>
2019-06-19 13:20:45 +0200

    Dodate funkcionalnosti

--- src/com/company/Pitanje.java
+++ src/com/company/Pitanje.java
@@ -1,7 +1,45 @@
 package com.company;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Pitanje implements Serializable {
+    String tekst;
+    ArrayList<String> odgovori;
+    int indexTacnog;
 
+    public Pitanje() {}
+
+    public int getIndexTacnog() {
+        return indexTacnog;
+    }
+
+    public void setIndexTacnog(int indexTacnog) {
+        this.indexTacnog = indexTacnog;
+    }
+
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+
+    public ArrayList<String> getOdgovori() {
+        return odgovori;
+    }
+
+    public void setOdgovori(ArrayList<String> odgovori) {
+        this.odgovori = odgovori;
+    }
+
+    @Override
+    public String toString() {
+        return "Pitanje{" +
+                "tekst='" + tekst + '\'' +
+                ", odgovori=" + odgovori +
+                ", indexTacnog=" + indexTacnog +
+                '}';
+    }
 }
--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -1,8 +1,34 @@
 package com.company;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Quiz implements Serializable {
     String naziv;
     ArrayList<Pitanje> pitanja;
+    public Quiz(){}
+
+    public ArrayList<Pitanje> getPitanja() {
+        return pitanja;
+    }
+
+    public void setPitanja(ArrayList<Pitanje> pitanja) {
+        this.pitanja = pitanja;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "naziv='" + naziv + '\'' +
+                ", pitanja=" + pitanja +
+                '}';
+    }
 }
